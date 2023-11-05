2dd55626af0fe401f71e423148c48890281bcc73
haiderali020692 <haiderali020692@gmail.com>
2023-11-05 23:32:54 +0500

    refurbishing

--- pages/About.js
+++ pages/About.js
@@ -8,9 +8,26 @@ const About = () => {
     <div className={`${styles.bggray} ${styles.fadeIn}`}>
       <Navbar />
       <div className={`${styles.mxauto} ${styles.maxw4xl} ${styles.p8}`}>
-        <h1 className={`${styles.text3xl} ${styles.fontbold} ${styles.mb4}`}>About Fiber Optics Australia</h1>
+        <h1 className={`${styles.text3xl} ${styles.fontbold} ${styles.mb4}`}>A Little About Us</h1>
         <p className={`${styles.textlg} ${styles.textgray600} ${styles.leading7}`}>
-          We have been providing fiber optic solutions in Australia for over 10 years. Our team of experts is dedicated to providing the best products and services to our customers.
+        Having a Slow or fault with your internet connection?.
+We have experienced technicians to resolve any fault and
+provide expert solution to boost up the internet speed.
+        </p>
+      </div>
+      <div className={`${styles.mxauto} ${styles.maxw4xl} ${styles.p8}`}>
+        <h1 className={`${styles.text3xl} ${styles.fontbold} ${styles.mb4}`}>What do we provide</h1>
+        <p className={`${styles.textlg} ${styles.textgray600} ${styles.leading7}`}>
+        We use High quality CAT 7 ethernet cables for networking
+throughout your house or Office with guarantee fast
+and reliable internet speed.
+        </p>
+      </div>
+      <div className={`${styles.mxauto} ${styles.maxw4xl} ${styles.p8}`}>
+        <h1 className={`${styles.text3xl} ${styles.fontbold} ${styles.mb4}`}>We also have</h1>
+        <p className={`${styles.textlg} ${styles.textgray600} ${styles.leading7}`}>
+        We have fully Licenced Security device installers to install
+security camera and alarm system at your home and office.
         </p>
       </div>
     </div>
