2386306fcf8ee775a962d3630281f08a1f80275c
haiderali020692 <haiderali020692@gmail.com>
2024-04-28 14:09:15 +0500

    Newest update and fixing misc

--- pages/Services.js
+++ pages/Services.js
@@ -1,60 +1,32 @@
-// pages/services.js
 import React from 'react';
 import Navbar from './Navbar';
 import styles from '../styles/Services.module.scss';
 import Link from 'next/link';
 import Footer from './Footer';
 import Image from 'next/image';
+import data from "../public/categories.json";
 
 const Services = () => {
+
+  const { responsive } = data.items;
+
   return (
     <div className={styles.servicesPage}>
       <Navbar />  
       <h1>Our Services</h1>
       <div className={styles.servicesList}>
-        <div className={styles.serviceCategory}>
-          <h2>Networking</h2>
-          <Image
-        src="/images/network.jpg"  // Path to your image inside the 'public' folder
-        alt="Networking Example"
-        width={500}
-        height={400}
-      />
-          <p>We offer professional networking solutions for your home or business.</p>
-          <Link href="../Networking">Read More</Link>
-        </div>
-        <div className={styles.serviceCategory}>
-          <h2>Security Cameras</h2>
-          <Image
-        src="/images/cctv.jpg"  // Path to your image inside the 'public' folder
-        alt="Security Camera Example"
-        width={500}
-        height={400}
-      />
-          <p>Enhance your security with our state-of-the-art security camera systems.</p>
-          <Link href="../SecurityCameras">Read More</Link>
-        </div>
-        <div className={styles.serviceCategory}>
-          <h2>Security Alarm Systems</h2>
-          <Image
-        src="/images/sec.jpg"  // Path to your image inside the 'public' folder
-        alt="Security Example Example"
-        width={500}
-        height={400}
-      />
-          <p>Protect your property with our advanced security alarm systems.</p>
-          <Link href="../SecuritySystem">Read More</Link>
-        </div>
-        <div className={styles.serviceCategory}>
-          <h2>Wall-Mounting</h2>
-          <Image
-        src="/images/wall.jpg"  // Path to your image inside the 'public' folder
-        alt="Networking Example"
-        width={500}
-        height={400}
-      />
-          <p>We provide wall-mounting services for TVs, monitors, and more.</p>
-          <Link href="../WallMounting">Read More</Link>
+        <div>
+      <ol className={styles.serviceCategory}>
+        {responsive.map((item) => (
+          <li key={item.id}>
+            <img src={item.imageUrl} alt="slides" width="1000" height="600"/>
+            <Link href={`/products/${item.slug}`}>
+            <h2>{item.title}</h2>
+            </Link>
+            <p>{item.text}</p>
+          </li>
+        ))}
+      </ol>
         </div>
       </div>
       <Footer />
