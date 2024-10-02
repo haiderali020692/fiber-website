22c145612cfe7b431c8797fa4e69d1eec0a6ab10
haiderali020692 <haiderali020692@gmail.com>
2024-10-03 02:46:15 +0500

    responsiveness at the end - false v3

--- pages/products/product/Slider.js
+++ pages/products/product/Slider.js
@@ -1,6 +1,6 @@
 // components/Carousel.js
 import Slider from "react-slick";
-
+import Image from "next/image"
 const Carousel = ({ images }) => {
   const settings = {
     dots: true,
@@ -18,7 +18,7 @@ const Carousel = ({ images }) => {
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index}>
-            <img src={image.src} alt={image.alt} className="carousel-image"/>
+            <Image src={image.src} alt={image.alt} className="carousel-image" width="400px"/>
           </div>
         ))}
       </Slider>
