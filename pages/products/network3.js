f39c277b60031e6c2b881eed0d4d16b7073a9bcd
Haider Ali <haiderali020692@gmail.com>
2024-12-03 03:37:47 +0500

    fullscreen

--- pages/products/network3.js
+++ pages/products/network3.js
@@ -1,29 +1,77 @@
 import React from 'react';
 import styles from '../../styles/product.module.scss';
+import Link from 'next/link'
+import { motion } from 'framer-motion';
 
 const Product = () => {
   return (
     <div className={styles.productPage}>
+      
+      <h1 style={{color: "white"}}>Access Points</h1>
+      <Link href={`/Networking`}><button className={styles.buyButton}>Back to Networking</button></Link>
+      <motion.div
+      initial={{ scale: 0 }}
+      animate={{ rotate: 360, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20
+      }}
+      >
       <div className={styles.productCard}>
         <img
           className={styles.productImage}
-          src="../../images/sec.jpg"
+          src="../../images/ap1.png"
           alt="Product"
         />
         <div className={styles.productDetails}>
-          <h1 className={styles.productTitle}>Amazing Product</h1>
+          <h1 className={styles.productTitle}>EAP110 TP-Link Wireless-N Ceiling Mount Access Point </h1>
           <p className={styles.productDescription}>
-            This product is top-notch too! It has everything you need to improve your life.
+            This product is top-notch! It has everything you need to improve your life.
           </p>
           <span className={styles.productPrice}>$99.99</span>
-          <button className={styles.buyButton}>Buy Now</button>
+          <Link href={`/products/product/wireless1`}><button className={styles.buyButton}>Check it out</button></Link>
         </div>
       </div>
+      </motion.div>
+      <motion.div
+      initial={{ scale: 0 }}
+      animate={{ rotate: 360, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20
+      }}
+      >
+      <div className={styles.productCard}>
+        <img
+          className={styles.productImage}
+          src="../../images/network.jpg"s
+          alt="Product"
+        />
+        <div className={styles.productDetails}>
+          <h1 className={styles.productTitle}>EAP110 TP-Link Wireless-N Ceiling Mount Access Point </h1>
+          <p className={styles.productDescription}>
+            This product is top-notch! It has everything you need to improve your life.
+          </p>
+          <span className={styles.productPrice}>$99.99</span>
+          <button className={styles.buyButton}>Call Us or Email!</button>
+        </div>
+      </div>
+      </motion.div>
     </div>
   );
 };
 
 export default Product;
+
+
+
+
+
+
+
+
 
 
 
