1bde0e8e8fa8e4c622776ec818dbb83f85eeb88c
Haider Ali <haiderali020692@gmail.com>
2024-12-09 03:38:37 +0500

    Images on About Page

--- pages/About.js
+++ pages/About.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import styles from '../styles/Home.module.scss';
 import Footer from './Footer';
 import { motion } from 'framer-motion';
+import Image from 'next/image'
 
 const About = () => {
   return (
@@ -19,7 +20,17 @@ const About = () => {
       }}
       >
       <div className={`${styles.mxauto} ${styles.maxw4xl} ${styles.p8}`}>
+        
         <h1 className={`${styles.text3xl} ${styles.fontbold} ${styles.mb4}`}>A Little About Us</h1>
+        <Image
+        className={styles.imageWrapper2}
+        src="/images/6015590.jpg" // Path to your image in the public folder
+        alt="about1"
+        width={600} // Desired width of the image
+        height={400} // Desired height of the image
+        priority // Ensures this image loads faster
+        
+        ></Image>
         <p className={`${styles.textlg} ${styles.textgray600} ${styles.leading7}`}>
         Having a Slow or fault with your internet connection?.
 We have experienced technicians to resolve any fault and
@@ -38,6 +49,14 @@ provide expert solution to boost up the internet speed.
       >
       <div className={`${styles.mxauto} ${styles.maxw4xl} ${styles.p8}`}>
         <h1 className={`${styles.text3xl} ${styles.fontbold} ${styles.mb4}`}>What do we provide</h1>
+        <Image
+        className={styles.imageWrapper2}
+        src="/images/5200686.jpg" // Path to your image in the public folder
+        alt="about3"
+        width={600} // Desired width of the image
+        height={400} // Desired height of the image
+        priority // Ensures this image loads faster
+        ></Image>
         <p className={`${styles.textlg} ${styles.textgray600} ${styles.leading7}`}>
         We use High quality CAT 7 ethernet cables for networking
 throughout your house or Office with guarantee fast
@@ -56,6 +75,14 @@ and reliable internet speed.
       >
       <div className={`${styles.mxauto} ${styles.maxw4xl} ${styles.p8}`}>
         <h1 className={`${styles.text3xl} ${styles.fontbold} ${styles.mb4}`}>We also have</h1>
+        <Image
+        className={styles.imageWrapper2}
+        src="/images/4117551.jpg" // Path to your image in the public folder
+        alt="about3"
+        width={600} // Desired width of the image
+        height={400} // Desired height of the image
+        priority // Ensures this image loads faster
+        ></Image>
         <p className={`${styles.textlg} ${styles.textgray600} ${styles.leading7}`}>
         We have fully Licenced Security device installers to install
 security camera and alarm system at your home and office.
